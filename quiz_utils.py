3d5b6838a54a912fd73dcb9ee392d97dc8cb7c0b
jianlins <jianlinshi.cn@gmail.com>
2017-07-16 06:41:08 +0000

    add quiz to 10

--- quiz_utils.py
+++ quiz_utils.py
@@ -51,6 +51,52 @@ def error_analyses_3():
 unremarkable. There is <span style="color: blue;">patchy opacity at the left lower lobe representing
 either atelectasis or consolidation</span>. No definite free air is identified.</body></html>'''))    
     
+def error_analyses_4():
+    option_explanations=OrderedDict()
+    option_explanations['A']='The last answer is correct.'
+    option_explanations['B']='The last answer is correct.'
+    option_explanations['C']='The last answer is correct.'
+    option_explanations['D']='Correct!'
+    single_choice(option_explanations,'Choose the best answer:',\
+                 HTML('''<html><body>If you see a rule for the DocumentClassifier only has one item: <b>"indicate_pneumonia"</b>, what does this rule means?:
+<ul>
+  <li><b>A</b>: If find an annotation with type "indicate_pneumonia", conclude 1.</li>
+  <li><b>B</b>: If find an annotation with any type, conclude "indicate_pneumonia".</li>
+  <li><b>C</b>: No matter what, always conclude "indicate_pneumonia".</li>
+  <li><b>D</b>: If no above rule is met, conclude "indicate_pneumonia".</li>
+</ul>  
+</body></html>'''))    
+
     
+def error_analyses_5():
+    option_explanations=OrderedDict()
+    option_explanations['A']='The last answer is correct.'
+    option_explanations['B']='The last answer is correct.'
+    option_explanations['C']='This answer is good, but the last one is more precise.'
+    option_explanations['D']='Correct!'
+    single_choice(option_explanations,'Choose the best answer:',\
+                 HTML('''<html><body>If you see a rule for the DocumentClassifier has two items: <b>"possible_diabetes&nbsp;&nbsp;&nbsp;&nbsp;high_glucose_test"</b>, what does this rule means?:
+<ul>
+  <li><b>A</b>: If find a string "high_glucose_test" in the document, conclude "possible_diabetes".</li>
+  <li><b>B</b>: If find an annotation with type "possible_diabetes", suggest "high_glucose_test".</li>
+  <li><b>C</b>: If find an annotation with type "high_glucose_test", conclude "possible_diabetes".</li>
+  <li><b>D</b>: If no above rule is met and find an annotation with type "high_glucose_test", conclude "indicate_pneumonia".</li>
+</ul>  
+</body></html>'''))
     
+def error_analyses_6():
+    option_explanations=OrderedDict()
+    option_explanations['A']='The last answer is correct.'
+    option_explanations['B']='The last answer is correct.'
+    option_explanations['C']='Try again.'
+    option_explanations['D']='Correct!'
+    single_choice(option_explanations,'Choose the best answer:',\
+                 HTML('''<html><body>If you see a rule for the DocumentClassifier has two items: <b>"possible_anemia&nbsp;&nbsp;&nbsp;&nbsp;anemia&nbsp;&nbsp;&nbsp;&nbsp;definite_existence&nbsp;&nbsp;&nbsp;&nbsp;probable_existence"</b>, what does this rule means?:
+<ul>
+  <li><b>A</b>: If find an annotation with type "anemia", conclude "possible_anemia".</li>
+  <li><b>B</b>: If find an annotation with type "anemia", "definite_existence" or "probable_existence", conclude "possible_anemia".</li>
+  <li><b>C</b>: If find an annotation with type "anemia" and with a modifier value either "definite_existence" or "probable_existence", conclude "possible_anemia".</li>
+  <li><b>D</b>: If find an annotation that has type "anemia" and its modifier values include both "definite_existence" and "probable_existence", conclude "possible_anemia".</li>
+</ul>  
+</body></html>'''))        
     
