1258b01d4df332b623c9ffea0aea91cfb469317a
jianlins <jianlinshi.cn@gmail.com>
2017-07-16 07:20:53 +0000

    add quiz for 11

--- quiz_utils.py
+++ quiz_utils.py
@@ -98,5 +98,54 @@ def error_analyses_6():
   <li><b>C</b>: If find an annotation with type "anemia" and with a modifier value either "definite_existence" or "probable_existence", conclude "possible_anemia".</li>
   <li><b>D</b>: If find an annotation that has type "anemia" and its modifier values include both "definite_existence" and "probable_existence", conclude "possible_anemia".</li>
 </ul>  
-</body></html>'''))        
+</body></html>'''))  
     
+def error_analyses_7():
+    option_explanations=OrderedDict()
+    option_explanations['A']='Try again.'
+    option_explanations['B']='Try again.'
+    option_explanations['C']='Try again.'
+    option_explanations['D']='Correct!'
+    single_choice(option_explanations,'Choose the best answer:',\
+                 HTML('''<html><body>When you doing error analyses, if you see following pyConText graph plot: <img src="data/Selection_008.png"> <br/> and following snippet:<img src="data/Selection_009.png">, what can you conclude?<br/>
+<ul>
+  <li><b>A</b>: This is a false positive error</li>
+  <li><b>B</b>: This is a false negative error.</li>
+  <li><b>C</b>: This is correct.</li>
+  <li><b>D</b>: Not enough information to make a conclusion.</li>
+</ul>  
+</body></html>'''))      
+    
+def error_analyses_8():
+    option_explanations=OrderedDict()
+    option_explanations['A']='Try again.'
+    option_explanations['B']='Try again.'
+    option_explanations['C']='Please don\'t waste your time.'
+    option_explanations['D']='Correct!'
+    single_choice(option_explanations,'Choose the best answer:',\
+                 HTML('''<html><body>In the question above, what may be the cause?<br/>
+<ul>
+  <li><b>A</b>: "AMBIVALENT_EXISTENCE" is excluded by DocumentClassifier</li>
+  <li><b>B</b>: "INDICATION" is excluded by pyConText.</li>
+  <li><b>C</b>: "pneumonia" is colored orange.</li>
+  <li><b>D</b>: "man" should not be a "DEFINITE_NEGATED_EXISTENCE" clue.</li>
+</ul>  
+</body></html>'''))      
+    
+    
+    
+def error_analyses_9():
+    option_explanations=OrderedDict()
+    option_explanations['A']='Try again.'
+    option_explanations['B']='Try again.'
+    option_explanations['C']='Please don\'t waste your time.'
+    option_explanations['D']='Correct!'
+    single_choice(option_explanations,'Choose the best answer:',\
+                 HTML('''<html><body>In the question above, what should we do next?<br/>
+<ul>
+  <li><b>A</b>: delete the rule that conclude "AMBIVALENT_EXISTENCE" as negative answer in our DocumentClassifier rule file</li>
+  <li><b>B</b>: add "man" in our target rule file.</li>
+  <li><b>C</b>: add "pneumonia" in our target rule file.</li>
+  <li><b>D</b>: remove the rule like "man&nbsp;&nbsp;&nbsp;&nbsp;DEFINITE_NEGATED_EXISTENCE&nbsp;...." in our modifier rule file.</li>
+</ul>  
+</body></html>'''))      
